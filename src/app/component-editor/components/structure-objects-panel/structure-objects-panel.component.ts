8eb80cf389e8e54910b3902e475ac5c28a69c7eb
Christophe <ch.vimbert@gmail.com>
2020-01-30 16:28:58 +0100

    Default panel change

--- src/app/component-editor/components/structure-objects-panel/structure-objects-panel.component.ts
+++ src/app/component-editor/components/structure-objects-panel/structure-objects-panel.component.ts
@@ -11,7 +11,7 @@ export class StructureObjectsPanelComponent implements OnInit {
 
   @Input() bounds: DOMRect;
 
-  selectedTabId = "graph";
+  selectedTabId = "scene-states";
   providers: { [key: string]: DataBank<any> } = {};
 
   constructor(
